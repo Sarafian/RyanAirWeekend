c604e78f7d294bbc00d7036d312de294ee8c0265
a.sarafian <dev1978@outlook.com>
2016-06-25 20:07:42 +0200

    Moved the export to json to pure .NET to reduce speed.

---
+++ VSS/ExportJson/ExportJson/Models/WeekendExcursionSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportJson.Models
+{
+    class WeekendExcursionSettings
+    {
+        public int OutboundEarliestFriday { get; set; }
+        public int OutboundLatestSaturday { get; set; }
+        public int InboundEarliestSunday { get; set; }
+        public int InboundLatestMonday { get; set; }
+    }
+}
